f22d0c4d820b071e5e9f5d096da46ad6a12d5913
Deathmax <cheahjunsiang@gmail.com>
2017-10-25 20:38:01 +0100

    Update to PRAW 5 since 3 has stopped working

--- flairbot.py
+++ flairbot.py
@@ -1,5 +1,7 @@
 import praw
 import requests
+import json
+import io
 from time import sleep
 from datetime import datetime
 
@@ -20,32 +22,31 @@ def getFlairCsv():
     return flairList
 
 
+def readConfig():
+    return json.load(open('config.json'))
+
+
 if __name__ == "__main__":
     printl('Starting bf-flair-bot')
-    user_agent = "linux:com.deathsnacks.bravefrontier.flair:0.1-alpha (by /u/Deathmax)"
-    r = praw.Reddit(user_agent=user_agent)
-    r.set_oauth_app_info(client_id="CID",
-                         client_secret="SECRET",
-                         redirect_uri="REDIRECT_URI")
-    
-    access_information = {u'access_token': u'ACCESS_TOKEN', 
-                          u'scope': set([u'wikiedit', u'privatemessages', u'modflair', u'identity', u'flair']), 
-                          u'refresh_token': u'REFRESH_TOKEN'}
-    
-    r.set_access_credentials(**access_information)
-    authenticated_user = r.get_me()
-    printl('Logged in as: ' + authenticated_user.name)
-    
+    config = readConfig()
+    r = praw.Reddit(user_agent='linux:com.deathsnacks.bravefrontier.flair:0.2-alpha (by /u/Deathmax)',
+                    client_id=config['client_id'],
+                    client_secret=config['client_secret'],
+                    redirect_uri=config['redirect_uri'],
+                    refresh_token=config['refresh_token'])
+
+    printl('Logged in as: ' + str(r.user.me()))
+
     while True:
         try:
             printl('Fetching flair CSV')
             flairList = getFlairCsv()
             printl('Getting messages')
-            messages = r.get_unread(False, False, limit=30)
+            messages = r.inbox.unread(mark_read=False, limit=30)
             for message in messages:
                 print message
-                if message.was_comment:
-                    message.mark_as_read()
+                if isinstance(message, praw.Comment):
+                    message.mark_read()
                     continue
                 if message.subject in flairList:
                     if (flairList[message.subject] == '*' or
@@ -53,7 +54,7 @@ if __name__ == "__main__":
                         flairText = message.body
                         if flairText.lower() is 'none':
                             flairText = ''
-                        r.get_subreddit('bravefrontier').set_flair(message.author, message.body, message.subject)
+                        r.subreddit('bravefrontier').flair.set(message.author, message.body, message.subject)
                         message.reply('_This is an automated message from the /r/bravefrontier bot._\n\nYour flair has been set to\'' + message.subject + '\'.')
                         printl('Set ' + message.author + ' flair to ' + message.subject + ' with text ' + flairText)
                     else:
